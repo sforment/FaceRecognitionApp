d02c759492bfe33852237b106659d5b91b83dd37
Stefan Formentano <StefanFormentano@gmail.com>
2019-03-29 20:39:48 -0500

    attempting to connect to heroku server

--- src/Components/Register/Register.js
+++ src/Components/Register/Register.js
@@ -23,7 +23,7 @@ class Register extends React.Component  {
 	}
 
 	onSumbitSignIn = () => {
-		fetch('http://localhost:3000/register', {
+		fetch('https://protected-sea-73806.herokuapp.com/register', {
 			method: 'post',
 			headers: {'Content-Type': 'application/json'},
 			body: JSON.stringify({
