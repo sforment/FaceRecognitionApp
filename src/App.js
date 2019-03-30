6566cad0f92dabb2e0ee2a99fccf6033b50261a2
Stefan Formentano <StefanFormentano@gmail.com>
2019-03-29 21:34:06 -0500

    deploy

--- src/App.js
+++ src/App.js
@@ -99,7 +99,7 @@ class App extends Component {
 			.then(response => response.json())
 			.then(response => {
 				if (response) {
-					fetch('https://protected-sea-73806.herokuapp.com/:3000/image', {
+					fetch('https://protected-sea-73806.herokuapp.com/image', {
 						method: 'put',
 						headers: {'Content-Type': 'application/json'},
 						body: JSON.stringify({
